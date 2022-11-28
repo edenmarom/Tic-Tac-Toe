73e34c2fbcd700b3b6a020fb202f8ab67efad154
chena <chena@jfrog.com>
2022-11-28 23:56:41 +0200

    improvements

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -18,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public static int moves = 0;
+
     // 0 - X
     // 1 - O
     int activePlayer = 0;
@@ -25,27 +27,19 @@ public class MainActivity extends AppCompatActivity {
     int[][] winPositions = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8},
             {0, 3, 6}, {1, 4, 7}, {2, 5, 8},
             {0, 4, 8}, {2, 4, 6}};
-    public static int moves = 0;
 
     public void playerTap(View view) {
         ImageView img = (ImageView) view;
         int tappedImage = Integer.parseInt(img.getTag().toString());
 
+        Button resetBtn = findViewById(R.id.resetButton);
+        ImageView status = findViewById(R.id.status);
+
         if (gameBoard[tappedImage] == 2) {
             moves++;
             gameBoard[tappedImage] = activePlayer;
+            drawImg(img, status);
 
-            if (activePlayer == 0) {
-                img.setImageResource(R.drawable.x);
-                activePlayer = 1;
-                ImageView status = findViewById(R.id.status);
-                status.setImageResource(R.drawable.oplay);
-            } else {
-                img.setImageResource(R.drawable.o);
-                activePlayer = 0;
-                ImageView status = findViewById(R.id.status);
-                status.setImageResource(R.drawable.xplay);
-            }
         }
         int winFlag = 0;
         int posIndex = 0;
@@ -55,38 +49,55 @@ public class MainActivity extends AppCompatActivity {
                     gameBoard[winPosition[1]] == gameBoard[winPosition[2]] &&
                     gameBoard[winPosition[0]] != 2) {
                 winFlag = 1;
-                int winnerImg;
-                if (gameBoard[winPosition[0]] == 0) {
-                    winnerImg = R.drawable.xwin;
-                } else {
-                    winnerImg = R.drawable.owin;
-                }
-                ImageView status = findViewById(R.id.status);
-                status.setImageResource(winnerImg);
-
-                Button resetBtn = findViewById(R.id.resetButton);
-                resetBtn.setVisibility(View.VISIBLE);
-                resetBtn.setOnClickListener(this::gameReset);
-
-                ImageView winLine = findViewById(R.id.winLine);
-
-                String uri = "@drawable/mark"+ posIndex;
-                int imageResource = getResources().getIdentifier(uri, null, getPackageName());
-                winLine.setImageResource(imageResource);
+                buildWinScreen(winPosition, posIndex, resetBtn, status);
             }
             posIndex++;
         }
         if (moves == 9 && winFlag == 0) {
-            ImageView status = findViewById(R.id.status);
-            status.setImageResource(R.drawable.nowin);
-
-            Button resetBtn = findViewById(R.id.resetButton);
-            resetBtn.setVisibility(View.VISIBLE);
-            resetBtn.setOnClickListener(this::gameReset);
+            buildDrawScreen(resetBtn, status);
         }
     }
 
-    public void gameReset(View view) {
+    public void drawImg(ImageView img, ImageView status) {
+        if (activePlayer == 0) {
+            img.setImageResource(R.drawable.x);
+            activePlayer = 1;
+            status.setImageResource(R.drawable.oplay);
+        } else {
+            img.setImageResource(R.drawable.o);
+            activePlayer = 0;
+            status.setImageResource(R.drawable.xplay);
+        }
+    }
+
+    public void buildWinScreen(int[] winPosition, int posIndex, Button resetBtn, ImageView status) {
+        int winnerImg;
+        if (gameBoard[winPosition[0]] == 0) {
+            winnerImg = R.drawable.xwin;
+        } else {
+            winnerImg = R.drawable.owin;
+        }
+
+        status.setImageResource(winnerImg);
+
+        resetBtn.setVisibility(View.VISIBLE);
+        resetBtn.setOnClickListener(view -> gameReset(view,resetBtn,status));
+
+        String uri = "@drawable/mark" + posIndex;
+        int imageResource = getResources().getIdentifier(uri, null, getPackageName());
+
+        ImageView winLine = findViewById(R.id.winLine);
+        winLine.setImageResource(imageResource);
+    }
+
+    public void buildDrawScreen(Button resetBtn, ImageView status) {
+        status.setImageResource(R.drawable.nowin);
+
+        resetBtn.setVisibility(View.VISIBLE);
+        resetBtn.setOnClickListener(view -> gameReset(view,resetBtn,status));
+    }
+
+    public void gameReset(View view, Button resetBtn, ImageView status) {
         activePlayer = 0;
         moves = 0;
         Arrays.fill(gameBoard, 2);
@@ -100,7 +111,7 @@ public class MainActivity extends AppCompatActivity {
         ((ImageView) findViewById(R.id.imageView7)).setImageResource(0);
         ((ImageView) findViewById(R.id.imageView8)).setImageResource(0);
         ((ImageView) findViewById(R.id.winLine)).setImageResource(0);
-        ImageView status = findViewById(R.id.status);
         status.setImageResource(R.drawable.xplay);
+        resetBtn.setVisibility(View.INVISIBLE);
     }
 }
